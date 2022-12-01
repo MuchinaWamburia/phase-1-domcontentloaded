efdcff457f0ff82b50a3771f6828dd9c888d2855
MuchinaWamburia <lucymuchina20@gmail.com>
2022-12-01 23:23:49 +0300

    completedAssignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", () =>{
+  console.log("AFTER Dom is loaded")
+  document.getElementById("text").innerHTML = "This is really cool!";
+})
